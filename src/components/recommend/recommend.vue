0eeafab602760719bce33d19d4588ade38872f3c
yaoqifa <yaoqifa@bilibili.com>
2017-12-13 18:25:19 +0800

    切换项目webpack服务等，抓取到歌单列表数据

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -23,17 +23,19 @@
 
 <script>
 import Slider from '@/base/slider/slider'
-import {getRecommend} from 'api/recommend'
+import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
   data () {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created () {
     this._getRecommend()
+    this._getDiscList()
   },
   components: {
     Slider
@@ -43,6 +45,13 @@ export default {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList () {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
         }
       })
     }
